b533edf39217cb657f544a5288b2a6856f459a8c
Alberto Padovan <37915405+albertopadovan@users.noreply.github.com>
2024-03-13 16:39:20 -0400

    Initialize examples folder

---
+++ Examples/toymodel/fom_class.py
@@ -0,0 +1,66 @@
+import numpy as np 
+import scipy.linalg as sciplin
+
+
+class full_order_model:
+        
+    def __init__(self,A2,A3,B,C):
+        
+        self.A2 = A2    # Linear tensor (i.e., a matrix)
+        self.A3 = A3    # Quadratic tesors (i.e., third-order)
+        self.B = B      # The input matrix
+        self.C = C      # The output matrix
+        
+        
+    def evaluate_fom_dynamics(self,t,q,u):
+        """
+            Evaluate the FOM dynamics: dx/dt = Ax + H:xx^T + fu(t),
+            where A is a second-order tensor, H a third-order tensor and 
+            fu is an scipy interpolator for the input 
+        """
+        
+        f = u.copy() if hasattr(u,"__len__") == True else u(t)
+        if np.linalg.norm(q) >= 1e4:    vec = 0*q
+        else:                           vec = self.A2@q + np.einsum('ijk,j,k',self.A3,q,q) + f
+        
+        return vec
+    
+    def evaluate_fom_adjoint(self,t,q,fQ):
+        """
+            Evaluate the adjoint of the FOM dynamics, where fQ is a 
+            scipy interpolator for the base flow
+        """
+        
+        if np.linalg.norm(q) >= 1e4:    vec = 0*q
+        else:                           vec = (self.A2 + np.einsum('ijk,j',self.A3,fQ(t)) + \
+                                               np.einsum('ijk,k',self.A3,fQ(t))).T@q 
+        
+        return vec
+    
+    def compute_output(self,q):
+        return self.C@q
+    
+    def compute_output_derivative(self,q):
+        return self.C
+    
+    def assemble_petrov_galerkin_tensors(self,Phi,Psi):
+        """
+            Compute the Petrov-Galerkin projection of the tensors 
+            A2, A3 and B (which define the FOM dynamics)
+        """
+        
+        n, r = Phi.shape
+        PhiF = Phi@sciplin.inv(Psi.T@Phi)
+        
+        A2r = Psi.T@self.A2@PhiF
+        A3r = np.zeros((r,r,r))
+        for i in range (r):
+            for j in range (r):
+                A3r[:,i,j] = Psi.T@np.einsum('kij,i,j',self.A3,PhiF[:,i],PhiF[:,j])
+                
+        Br = Psi.T@self.B 
+        Cr = self.compute_output(PhiF)
+        
+        return (A2r, A3r), (Br, Cr)
+        
+        
---
+++ Examples/toymodel/main.py
@@ -0,0 +1,236 @@
+import numpy as np 
+import scipy 
+import matplotlib.pyplot as plt
+from mpi4py import MPI
+
+from scipy.interpolate import interp1d
+from scipy.integrate import solve_ivp
+import sys
+
+import pymanopt
+import pymanopt.manifolds as manifolds
+import pymanopt.optimizers as optimizers
+from pymanopt.tools.diagnostics import check_gradient
+from pymanopt.optimizers.line_search import myAdaptiveLineSearcher
+
+plt.rcParams.update({"font.family":"serif","font.sans-serif":["Computer Modern"],'font.size':18,'text.usetex':True})
+plt.rc('text.latex',preamble=r'\usepackage{amsmath}')
+
+sys.path.append("/Users/alberto/Documents/SIAM_nitrom/OptimizationFunctions/")
+import classes
+import nitrom_functions 
+import opinf_functions as opinf_fun
+import troop_functions
+import fom_class
+
+
+cPOD, cOI, cTR, cOPT = '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3'
+lPOD, lOI, lTR, lOPT = 'solid', 'dotted', 'dashed', 'dashdot'
+
+
+#%% Instantiate the full-order model class
+
+n = 3 
+beta = 20
+A2 = np.diag([-1,-2,-5])
+A3 = np.zeros((3,3,3))
+A3[:,:,-1] = np.diag([beta,beta,0.0])
+B = np.ones((3,1))
+C = np.ones((1,3))
+time = np.linspace(0,10,num=20)
+
+fom = fom_class.full_order_model(A2,A3,B,C)
+
+#%% Generate training trajectories and save to file
+
+traj_path = "./trajectories/"
+
+fname_traj = traj_path + "traj_%03d.txt"
+fname_weight = traj_path + "weight_%03d.txt"
+fname_forcing = traj_path + "forcing_%03d.txt"
+fname_deriv = traj_path + "deriv_%03d.txt"
+fname_time = traj_path + "time.txt"
+
+n_traj = 4
+
+betas = np.asarray([0.01,0.1,0.2,0.248])
+n_traj = len(betas)
+weights = np.zeros(len(betas))
+for k in range (len(betas)):
+    u = betas[k]*np.ones(3)
+    sol = solve_ivp(fom.evaluate_fom_dynamics,[0,time[-1]],np.zeros(3),'RK45',t_eval=time,args=(u,))
+    
+    dX = np.zeros((3,len(time)))
+    for j in range (sol.y.shape[-1]):
+        dX[:,j] = fom.evaluate_fom_dynamics(time[j],sol.y[:,j],u) - u
+    
+    
+    id_ss = np.asarray([-betas[k]/(-1+4*betas[k]),-betas[k]/(-2+4*betas[k]),betas[k]/5])
+    weights[k] = np.linalg.norm(fom.compute_output(id_ss))**2
+    
+    np.savetxt(fname_traj%k,sol.y)
+    np.savetxt(fname_deriv%k,dX)
+    np.savetxt(fname_weight%k,[weights[k]])
+    np.savetxt(fname_forcing%k,u)
+    
+
+np.savetxt(traj_path + "time.txt",time)
+
+
+#%% Compute POD model 
+
+pool_inputs = (MPI.COMM_WORLD, n_traj, fname_traj, fname_time)
+pool_kwargs = {'fname_steady_forcing':fname_forcing,'fname_weights':fname_weight,'fname_derivs':fname_deriv}
+pool = classes.mpi_pool(*pool_inputs,**pool_kwargs)
+
+
+r = 2               # ROM dimension
+poly_comp = [1,2]   # Model with a linear part and a quadratic part
+
+
+Phi_pod, _ = opinf_fun.perform_POD(pool,2)
+Psi_pod = Phi_pod.copy()
+tensors_pod, _ = fom.assemble_petrov_galerkin_tensors(Phi_pod,Psi_pod)
+
+
+#%% Compute NiTROM model 
+
+which_trajs = np.arange(0,pool.my_n_traj,1)
+which_times = np.arange(0,pool.n_snapshots,1)
+leggauss_deg = 5
+nsave_rom = 10
+
+opt_obj_inputs = (pool,which_trajs,which_times,leggauss_deg,nsave_rom,[1,2])
+# opt_obj_kwargs = {'stab_promoting_pen':1e-2,'stab_promoting_tf':20,'stab_promoting_ic':(np.random.randn(r),)}
+
+
+opt_obj = classes.optimization_objects(*opt_obj_inputs)
+
+
+St = manifolds.Stiefel(n,r)
+Gr = manifolds.Grassmann(n,r)
+Euc_rr = manifolds.Euclidean(r,r)
+Euc_rrr = manifolds.Euclidean(r,r,r)
+
+M = manifolds.Product([Gr,St,Euc_rr,Euc_rrr])
+cost, grad, hess = nitrom_functions.create_objective_and_gradient(M,opt_obj,pool,fom)
+problem = pymanopt.Problem(M,cost,euclidean_gradient=grad)
+check_gradient(problem,x=[Phi_pod,Psi_pod,*tensors_pod])
+# check_gradient(problem,x=result.point)
+
+
+line_searcher = myAdaptiveLineSearcher(contraction_factor=0.5,sufficient_decrease=0.85,max_iterations=25,initial_step_size=1)
+optimizer = optimizers.ConjugateGradient(max_iterations=1000,min_step_size=1e-20,max_time=3600,line_searcher=line_searcher,log_verbosity=1)
+
+
+point = (Phi_pod,Psi_pod) + tensors_pod
+result = optimizer.run(problem,initial_point=point)
+check_gradient(problem,x=result.point)
+
+Phi_nit = result.point[0]
+Psi_nit = result.point[1]
+Phi_nit = Phi_nit@scipy.linalg.inv(Psi_nit.T@Phi_nit)
+tensors_nit = tuple(result.point[2:])
+
+
+itervec_nit = result.log["iterations"]["iteration"]
+costvec_nit = result.log["iterations"]["cost"]
+gradvec_nit = result.log["iterations"]["gradient_norm"]
+
+#%%
+
+plt.figure()
+plt.plot(itervec_nit,costvec_nit)
+
+ax = plt.gca()
+ax.set_yscale('log')
+
+#%% Compute TrOOP model
+
+optimizer = optimizers.ConjugateGradient(max_iterations=200,min_step_size=1e-20,max_time=3600,line_searcher=line_searcher,log_verbosity=1)
+
+M = manifolds.Product([Gr,Gr])
+cost_troop, grad_troop, _ = troop_functions.create_objective_and_gradient(M,opt_obj,pool,fom)
+problem = pymanopt.Problem(M,cost_troop,euclidean_gradient=grad_troop)
+
+
+result = optimizer.run(problem,initial_point=(Phi_pod,Psi_pod))
+check_gradient(problem,x=result.point)
+
+Phi_tr = result.point[0]
+Psi_tr = result.point[1]
+Phi_tr = Phi_tr@scipy.linalg.inv(Psi_tr.T@Phi_tr)
+
+tensors_tr, _ = fom.assemble_petrov_galerkin_tensors(Phi_tr,Psi_tr)
+
+itervec_tr = result.log["iterations"]["iteration"]
+costvec_tr = result.log["iterations"]["cost"]
+gradvec_tr = result.log["iterations"]["gradient_norm"]
+
+
+#%% Compute OpInf model
+
+lam = np.logspace(-6.4,-3,num=100)
+cost_oi = []
+for (count,l) in enumerate(lam):
+    tensors_opinf = opinf_fun.operator_inference(pool,Phi_pod,poly_comp,[0.0,l])
+    point = (Phi_pod,Psi_pod) + tensors_opinf
+    cost_oi.append(cost(*point))
+
+plt.figure()
+plt.plot(lam,cost_oi)
+
+#%%
+lambdas = [0.0,lam[np.argmin(cost_oi)]]
+tensors_oi = opinf_fun.operator_inference(pool,Phi_pod,poly_comp,lambdas)
+
+#%%
+tk = np.linspace(0,30,num=10000)
+uk = 0.5*(np.sin(tk) + np.cos(2*tk))
+
+t_eval = np.linspace(0,30,num=1000)
+
+
+fu = scipy.interpolate.interp1d(tk,np.outer(B,uk),kind='linear',fill_value='extrapolate')
+sol_fom = (solve_ivp(fom.evaluate_fom_dynamics,[0,tk[-1]],np.zeros(3),'RK45',t_eval=t_eval,args=(fu,))).y
+
+
+
+fu = scipy.interpolate.interp1d(tk,np.outer(Psi_pod.T@B,uk),kind='linear',fill_value='extrapolate')
+sol_pod = Phi_pod@(solve_ivp(opt_obj.evaluate_rom_rhs,[0,tk[-1]],np.zeros(r),'RK45',t_eval=t_eval,args=(fu,) + tensors_pod)).y
+
+
+fu = scipy.interpolate.interp1d(tk,np.outer(Psi_pod.T@B,uk),kind='linear',fill_value='extrapolate')
+sol_oi = Phi_pod@(solve_ivp(opt_obj.evaluate_rom_rhs,[0,tk[-1]],np.zeros(r),'RK45',t_eval=t_eval,args=(fu,) + tensors_oi)).y
+
+
+fu = scipy.interpolate.interp1d(tk,np.outer(Psi_tr.T@B,uk),kind='linear',fill_value='extrapolate')
+sol_tr = Phi_tr@(solve_ivp(opt_obj.evaluate_rom_rhs,[0,tk[-1]],np.zeros(r),'RK45',t_eval=t_eval,args=(fu,) + tensors_tr)).y
+
+
+fu = scipy.interpolate.interp1d(tk,np.outer(Psi_nit.T@B,uk),kind='linear',fill_value='extrapolate')
+sol_nit = Phi_nit@(solve_ivp(opt_obj.evaluate_rom_rhs,[0,tk[-1]],np.zeros(r),'RK45',t_eval=t_eval,args=(fu,) + tensors_nit)).y
+
+
+plt.figure()
+plt.plot(t_eval,fom.compute_output(sol_fom)[0,],color='k',linewidth=2)
+plt.plot(t_eval,fom.compute_output(sol_pod)[0,],color=cPOD,linestyle=lPOD,linewidth=2)
+plt.plot(t_eval,fom.compute_output(sol_oi)[0,],color=cOI,linestyle=lOI,linewidth=2)
+plt.plot(t_eval,fom.compute_output(sol_tr)[0,],color=cTR,linestyle=lTR,linewidth=2)
+plt.plot(t_eval,fom.compute_output(sol_nit)[0,],color=cOPT,linestyle=lOPT,linewidth=2)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
