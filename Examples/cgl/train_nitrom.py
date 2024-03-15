52d45acff60b7536b3060e5e8347308cb3b203fd
Alberto Padovan <37915405+albertopadovan@users.noreply.github.com>
2024-03-15 12:56:29 -0400

    Modify train_nitrom.py

--- Examples/cgl/train_nitrom.py
+++ Examples/cgl/train_nitrom.py
@@ -2,31 +2,22 @@ import numpy as np
 import scipy 
 from mpi4py import MPI
 
-from scipy.interpolate import interp1d
-from scipy.integrate import solve_ivp
-import sys
+import sys, os
 
 import pymanopt
 import pymanopt.manifolds as manifolds
 import pymanopt.optimizers as optimizers
-from pymanopt.tools.diagnostics import check_gradient
-from pymanopt.optimizers.line_search import myAdaptiveLineSearcher
 
+sys.path.append(os.path.abspath("../../OptimizationFunctions/"))
 
-sys.path.append("/scratch1/09103/padovan3/NiTROM_tests/OptimizationFunctions")
+from my_pymanopt_classes import myAdaptiveLineSearcher
+
 import classes
 import nitrom_functions 
-import opinf_functions as opinf_fun
-import troop_functions
 import fom_class_cgl
 
 
-cPOD, cOI, cTR, cOPT = '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3'
-lPOD, lOI, lTR, lOPT = 'solid', 'dotted', 'dashed', 'dashdot'
-
-
-#%% # Instantiate CGL class and CGL time-stepper class
-
+# Define FOM
 L = 100
 nx = 256
 x = np.linspace(-L/2,L/2,num=nx,endpoint=True) 
@@ -40,7 +31,7 @@ fom = fom_class_cgl.CGL(x,nu,gamma,mu0,mu2,a)
 
 
 dt = 1e-2
-T = 500
+T = 1000
 time = dt*np.arange(0,T//dt,1)
 tstep_cgl = fom_class_cgl.time_step_cgl(fom,time)
 
@@ -48,7 +39,7 @@ nsave = 100
 tsave = time[::nsave]
 
 
-#%%
+
 traj_path = "./trajectories/"
 
 fname_traj = traj_path + "traj_%03d.txt"
@@ -57,36 +48,10 @@ fname_deriv = traj_path + "deriv_%03d.txt"
 fname_time = traj_path + "time.txt"
 
 
-amps = [0.01,0.1,1.0,-1.0]
-qIC = fom.B.copy()
-
-q_ = np.zeros((qIC.shape[0],qIC.shape[-1]*len(amps)))
-for k in range (len(amps)):
-    q_[:,k*qIC.shape[-1]:(k+1)*qIC.shape[-1]] = amps[k]*qIC
-
-qIC = q_.copy()
-n_traj = qIC.shape[-1]
-
-for k in range (n_traj):
-        
-    print("Running simulation %d/%d"%(k,n_traj))
-    
-    Qkj, Ykj, tsave = tstep_cgl.time_step(fom,qIC[:,k],nsave)     
-    dQkj = np.zeros_like(Qkj)
-    for j in range (Qkj.shape[-1]):
-        dQkj[:,j] = fom.evaluate_fom_dynamics(0.0, Qkj[:,j], np.zeros(Qkj.shape[0]))
-        
-    weight = np.mean(np.linalg.norm(Ykj,axis=0)**2)
-    
-    np.savetxt(fname_traj%k,Qkj)
-    np.savetxt(fname_deriv%k,dQkj)
-    np.savetxt(fname_weight%k,[weight])
-
-np.savetxt(traj_path + "time.txt",tsave)
-#%% Compute POD ROM
+n_traj = 8
 
 pool_inputs = (MPI.COMM_WORLD, n_traj, fname_traj, fname_time)
-pool_kwargs = {'fname_weights':fname_weight,'fname_derivs':fname_deriv}
+pool_kwargs = {'fname_weights':fname_weight}
 pool = classes.mpi_pool(*pool_inputs,**pool_kwargs)
 
 
@@ -95,20 +60,94 @@ r = 5               # ROM dimension
 poly_comp = [1,3]   # Model with a linear part and a quadratic part
 
 
-Phi_pod, sig = opinf_fun.perform_POD(pool,r)
-Psi_pod = Phi_pod.copy()
-tensors_pod, _ = fom.assemble_petrov_galerkin_tensors(Phi_pod,Psi_pod)
+# Phi_pod = np.load("data/Phi_pod.npy")
+# A2_oi = np.load("data/A2_oi.npy")
+# A4_oi = np.load("data/A4_oi.npy").reshape((r,r,r,r))
+# tensors_oi = (A2_oi,A4_oi)
+
+Phi_nit = np.load("data/Phi_nit.npy")
+Psi_nit = np.load("data/Psi_nit.npy")
+A2_nit = np.load("data/A2_nit.npy")
+A4_nit = np.load("data/A4_nit.npy").reshape((r,r,r,r))
+tensors_nit = (A2_nit,A4_nit)
 
 
-#%% Compute Operator Inference model (need NiTROM cost function to select l2 penalty)
+which_trajs = np.arange(0,pool.my_n_traj,1)
+which_times = np.arange(0,pool.n_snapshots,1)
+leggauss_deg = 5
+nsave_rom = 10
 
-lambdas = [0.0,1e9]
-tensors_oi = opinf_fun.operator_inference(pool,Phi_pod,poly_comp,lambdas)
+St = manifolds.Stiefel(n,r)
+Gr = manifolds.Grassmann(n,r)
+Euc_rr = manifolds.Euclidean(r,r)
+Euc_rrrr = manifolds.Euclidean(r,r,r,r)
+M = manifolds.Product([Gr,St,Euc_rr,Euc_rrrr])
+
+line_searcher = myAdaptiveLineSearcher(contraction_factor=0.4,sufficient_decrease=0.1,max_iterations=25,initial_step_size=1)
+# point = (Phi_pod,Phi_pod) + tensors_oi
+point = (Phi_nit,Psi_nit) + tensors_nit
 
 
-np.save("data/Phi_pod.npy",Phi_pod)
-np.save("data/A2_oi.npy",tensors_oi[0])
-np.save("data/A4_oi.npy",tensors_oi[1].reshape((r,r**3)))
+if pool.rank == 0:  verb = 2
+else:               verb = 0
+
+k0 = 0
+kouter = 50
 
 
+if k0 == 0:
+    costvec_nit = []
+    gradvec_nit = []
     
+for k in range (k0,k0+kouter):
+    
+    if np.mod(k,2) == 0:    which_fix = 'fix_bases'
+    else:                   which_fix = 'fix_tensors'
+
+    opt_obj_inputs = (pool,which_trajs,which_times,leggauss_deg,nsave_rom,poly_comp)
+    opt_obj_kwargs = {'which_fix':which_fix}
+    opt_obj = classes.optimization_objects(*opt_obj_inputs,**opt_obj_kwargs)
+    
+    if pool.rank == 0:
+        print("Optimizing (%d/%d) with which_fix = %s"%(k+1,kouter,opt_obj.which_fix))
+    
+    cost, grad, hess = nitrom_functions.create_objective_and_gradient(M,opt_obj,pool,fom)
+    problem = pymanopt.Problem(M,cost,euclidean_gradient=grad)
+    optimizer = optimizers.ConjugateGradient(max_iterations=5,min_step_size=1e-20,max_time=3600,\
+                                             line_searcher=line_searcher,log_verbosity=1,verbosity=verb)
+    result = optimizer.run(problem,initial_point=point)
+    point = result.point
+    
+    itervec_nit_k = result.log["iterations"]["iteration"]
+    costvec_nit_k = result.log["iterations"]["cost"]
+    gradvec_nit_k = result.log["iterations"]["gradient_norm"]
+    
+    if k == 0:    
+        costvec_nit.extend(costvec_nit_k) 
+        gradvec_nit.extend(gradvec_nit_k) 
+    else:         
+        costvec_nit.extend(costvec_nit_k[1:]) 
+        gradvec_nit.extend(gradvec_nit_k[1:]) 
+
+# check_gradient(problem,x=[Phi_pod,Phi_pod,*tensors_oi])
+# check_gradient(problem,x=result.point)
+
+Phi_nit = result.point[0]
+Psi_nit = result.point[1]
+Phi_nit = Phi_nit@scipy.linalg.inv(Psi_nit.T@Phi_nit)
+tensors_nit = tuple(result.point[2:])
+
+if pool.rank == 0:
+    np.save("data/Phi_nit.npy",Phi_nit)
+    np.save("data/Psi_nit.npy",Psi_nit)
+    np.save("data/A2_nit.npy",tensors_nit[0])
+    np.save("data/A4_nit.npy",tensors_nit[1].reshape((r,r**3)))
+
+
+
+
+
+
+
+
+
